d49f0a50a63b9bfd28641f8ed5f4861323d109a6
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-05-31 06:50:02 +0545

    1st

--- music/models.py
+++ music/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    #on_delete=models.CASCADE --> if album is deleted song is deleted
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=200)
