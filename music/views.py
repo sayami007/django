577b8b881e9ffffed2690cb6bbeeaeff64428f13
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-05-31 13:36:45 +0545

    Added Views

--- music/views.py
+++ music/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>this is music</h1")
-    
+    all_albums = Album.objects.all();
+    html = ''
+    for album in all_albums:
+        url = '/music/'+str(album.id)+'/'
+        html += '<a href="'+url+'">'+album.album_title+'</a><br>'
+    return HttpResponse(html)
+
+def detail(request,album_id):
+    return HttpResponse("<h1>Details for Album id :</h2>" + str(album_id))
